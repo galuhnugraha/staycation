3386d6073191a472dc7a220f9929e3a68138d09c
Galuh Nugraha <galuhnugraha28@gmail.com>
2020-10-04 11:56:36 +0700

    deploy heroku

---
+++ routes/api.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const {upload} = require('../middlewares/multer');
+const apiController = require('../controllers/apiController');
+
+//router dashboard
+router.get('/landing-page',apiController.landingPage);
+router.get('/detail-page/:id',apiController.detailPage);
+router.post('/booking-page',upload,apiController.bookingPage);
+
+module.exports = router;
+
