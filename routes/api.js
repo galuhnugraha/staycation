380ee07e0907f84a9efbe239291f1982d83bd7b9
Galuh Nugraha <galuhnugraha28@gmail.com>
2020-10-12 14:28:51 +0700

    remove cors

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,7 @@ const {upload} = require('../middlewares/multer');
 const apiController = require('../controllers/apiController');
 
 //router dashboard
-router.get('/landing-page',apiController.landingPage, 
-res.header("Access-Control-Allow-Origin","*"),
-res.header("Access-Control-Allow-Credentials",true));
+router.get('/landing-page',apiController.landingPage);
 router.get('/detail-page/:id',apiController.detailPage);
 router.post('/booking-page',upload,apiController.bookingPage);
 
