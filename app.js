2fb56ca0ef09a492d644a991a12931160cc3072e
Galuh Nugraha <galuhnugraha28@gmail.com>
2020-10-12 15:04:24 +0700

    add cors backend

--- app.js
+++ app.js
@@ -38,6 +38,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req,res,next) => {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,OPTIONS,PATH');
+  res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+  next()
+})
 app.use('/sb-admin-2',express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
 // admin
 app.use('/admin',adminRouter);
